4bfeef6dc318070d88aa4bcef8c5b8671074d564
Ylan <hei.nalisoa@gmail.com>
2022-09-14 09:22:22 +0300

    add cross origin

--- src/main/java/school/hei/haapi/endpoint/rest/controller/EventController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/EventController.java
@@ -2,6 +2,7 @@ package school.hei.haapi.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import java.util.List;
 @Controller
 @AllArgsConstructor
 @RequestMapping("/event")
+@CrossOrigin("http://localhost:3000")
 public class EventController {
 
   private final EventService eventService;
--- src/main/java/school/hei/haapi/endpoint/rest/controller/HealthController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/HealthController.java
@@ -1,9 +1,11 @@
 package school.hei.haapi.endpoint.rest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class HealthController {
 
   @GetMapping(value = "/ping", produces = "text/plain")
